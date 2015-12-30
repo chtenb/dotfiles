c29d14aa0b617deba4e718e12dc2a0c871053b2d
Chiel ten Brinke <chiel.tenbrinke@intellimagic.com>
2015-12-30 14:55:20 +0100

    Fix replace script.

--- replace.pl
+++ replace.pl
@@ -2,13 +2,13 @@ use strict;
 use warnings;
 use feature qw(switch);
 no warnings 'experimental::smartmatch';
+use Term::ANSIColor;
+use Term::ANSIColor 2.01 qw(colorstrip);
 
-sub writeline {
-    print ((join ', ', @_) ."\n");
-}
+sub writeline { print ((join ' ', @_) ."\n"); }
 
 sub help {
-    writeline 'USAGE: replace <FIND_PATTERN> <SUBSTITUTE_PATTERN> <FILES|DIRECTORIES> [including <PATTERNS>] [exluding <PATTERNS>] [now]';
+    writeline 'USAGE: replace <FIND_PATTERN> <SUBSTITUTE_PATTERN> <FILES|DIRECTORIES> [including <PATTERNS>] [excluding <PATTERNS>] [now]';
     writeline '';
     writeline 'The patterns are treated as Perl regex patterns and the files and directory patterns are treated like the corresponding grep arguments.';
     writeline 'If the "now" flag is not given, replace will run in dry mode';
@@ -26,6 +26,7 @@ my @where = ();
 my @include = ();
 my @exclude = ();
 my $dry = 1;
+
 
 # Retrieve config from commandline arguments
 my $current_command = \@where;
@@ -47,35 +48,52 @@ if (not $find or not $subst or not @where) {
     help;
 }
 
+my $escaped_find = quotemeta $find;
+my $escaped_subst = quotemeta $subst;
 my $escaped_include = join ' ', map { sprintf "--include=%s", quotemeta $_ } @include;
 my $escaped_exclude = join ' ', map { sprintf "--exclude=%s", quotemeta $_ } @exclude;
 my $escaped_where = join ' ', map { quotemeta $_ } @where;
-my $escaped_expr = quotemeta $find;
-my $escaped_subst = quotemeta $subst;
 
-writeline "Input interpretation: replace $escaped_expr with $escaped_subst in $escaped_where $escaped_include $escaped_exclude";
+writeline "Input interpretation: replace $escaped_find with $escaped_subst in $escaped_where $escaped_include $escaped_exclude";
+
 
 # Do shit
 if ($dry) {
-    my $grep_command = "grep --color=always -rHP $escaped_expr $escaped_where $escaped_include $escaped_exclude";
+    my $grep_command = "grep -rHP $escaped_find $escaped_where $escaped_include $escaped_exclude";
     writeline "Grep command: $grep_command";
     my $grep_out = `$grep_command`;
+    writeline $grep_out;
     my @lines = split('\n', $grep_out);
+
 
     for my $line (@lines) {
         # Retrieve grep info
         my ($file, @text) = split /\:/, $line;
-        writeline $file;
-        my $text = join '', @text;
-        writeline "OLD: $text";
 
-        # Substitute and print
-        $text =~ s/$find/$subst/g;
+        print color('magenta');
+        writeline colorstrip($file);
+        print color('reset');
+
+        #print color('cyan');
+        #writeline 'Patterns', $find, $subst;
+        #print color('reset');
+
+        my $text = join ':', @text;
+        my $printvar;
+        my $red = color('red');
+        my $reset = color('reset');
+        ($printvar = $text) =~ s/($find)/$red$1$reset/;
+        writeline "OLD: $printvar";
+
+        $text =~ s/$find/qq("$red$subst$reset")/gee;
         writeline "NEW: $text";
     }
 }
 else {
-    my $grep_command = "grep -rlP $escaped_expr $escaped_where $escaped_include $escaped_exclude";
+    my $grep_command = "grep -rlP $escaped_find $escaped_where $escaped_include $escaped_exclude";
     my @files = `$grep_command`;
-    exec 'perl', '-pi', '-e', "s/$find/$subst/g", @files;
+    my $files = (join ' ', (map quotemeta, (split "\n", (join '', @files))));
+    my $perl_command = "perl -pi -e 's/$find/$subst/g' $files";
+    writeline 'Perl replacement command: ', $perl_command;
+    `$perl_command`;
 }
