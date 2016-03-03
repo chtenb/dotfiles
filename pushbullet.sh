3624e3e1ff63fa347a211cf8cd47875388a7fea2
Chiel ten Brinke <chiel.tenbrinke@intellimagic.com>
2016-03-03 17:27:30 +0100

    Add pushbullet bash script.

---
+++ pushbullet.sh
@@ -0,0 +1,15 @@
+# Get token
+token=$1; shift
+
+# Get command
+command="$@"
+echo "Executing command '$command'"
+
+# Execute command
+echo $@ | bash
+returncode=$?
+
+# Push notification
+blackhole=`curl -s -u $token: https://api.pushbullet.com/v2/pushes -d type=note -d title="Command '$command' has finished."`
+
+exit $returncode
