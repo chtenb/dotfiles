df6b3afe912bb3799cfcc86a7e127cd1053447c1
Chiel ten Brinke <ctenbrinke@gmail.com>
2018-07-30 16:52:10 +0200

    add install

---
+++ install.bat
@@ -0,0 +1,6 @@
+mklink %UserProfile%\_vsvimrc %UserProfile%\dotfiles\_vsvimrc
+mklink %UserProfile%\.vimrc %UserProfile%\dotfiles\.vimrc
+mklink %UserProfile%\.vimrc.bare %UserProfile%\dotfiles\.vimrc.bare
+mklink %UserProfile%\.bash_aliases %UserProfile%\dotfiles\.bash_aliases
+mklink %UserProfile%\.bash_profile %UserProfile%\dotfiles\.bash_profile
+mklink %UserProfile%\.gitconfig %UserProfile%\dotfiles\.gitconfig-intellimagic
