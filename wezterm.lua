84d1b75b69e7c44c96ce8a284c058fc869d3529e
chtenb <ctenbrinke@gmail.com>
2022-04-15 23:06:42 +0200

    adaptable git colors

--- wezterm.lua
+++ wezterm.lua
@@ -121,8 +121,6 @@ end)
 
 return {
     window_close_confirmation = "NeverPrompt",
-    -- initial_cols = 1000,
-    -- initial_rows = 1000,
     hide_tab_bar_if_only_one_tab = true,
 
     font = wezterm.font_with_fallback({"Cascadia Code", "Cascadia Code,FiraCode Nerd Font"}),
@@ -130,31 +128,4 @@ return {
     -- for example, this selects a Bold, Italic font variant.
     -- font = wezterm.font("JetBrains Mono", {weight="Bold", italic=true})
     font_size = 10.0,
-    use_fancy_tab_bar = false,
-
-    -- window_decorations = "RESIZE",
-    window_frame = {
-        -- The font used in the tab bar.
-        -- Roboto Bold is the default; this font is bundled
-        -- with wezterm.
-        -- Whatever font is selected here, it will have the
-        -- main font setting appended to it to pick up any
-        -- fallback fonts you may have used there.
-        font = wezterm.font({
-            family = "Roboto",
-            weight = "Bold"
-        }),
-
-        -- The size of the font in the tab bar.
-        -- Default to 10. on Windows but 12.0 on other systems
-        font_size = 12.0,
-
-        -- The overall background color of the tab bar when
-        -- the window is focused
-        active_titlebar_bg = "#333333",
-
-        -- The overall background color of the tab bar when
-        -- the window is not focused
-        inactive_titlebar_bg = "#333333"
-    },
 }
