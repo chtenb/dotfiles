ee2e239d652b5f48ac57a814c4bbb357ea0d8b3a
chtenb <ctenbrinke@gmail.com>
2025-02-08 10:45:43 +0100

    .

--- wezterm/config/font.lua
+++ wezterm/config/font.lua
@@ -2,7 +2,12 @@
 local wezterm = require "wezterm"
 
 -- === <= |> =>
-local monaspace_features = { 'calt=1', 'ss01=1', 'ss02=0', 'ss03=1', 'ss04=1', 'ss05=1', 'ss06=1', 'ss07=1', 'ss08=1', 'ss09=0', 'liga=1', 'cv60=0' }
+local monaspace_features = { 'calt=1', 'ss01=1', 'ss02=0', 'ss03=1', 'ss04=1', 'ss05=1', 'ss06=1', 'ss07=1', 'ss08=1', 'ss09=0', 'liga=1'
+  , 'cv01=0' -- zero styles
+  , 'cv10=0' -- l i alternates (Neon, Argon, Xenon, Radon)
+  , 'cv11=0' -- j f r t alternates (Neon, Argon)
+  , 'cv60=0' -- forces the <= pair to render in a fashion that matches => instead of swapping for ≤.
+  }
 -- local monaspace_features = { 'calt=1', 'ss01=1', 'ss02=0', 'ss03=1', 'ss04=1', 'ss05=1', 'ss06=1', 'ss07=1', 'ss08=1', 'ss09=1', 'liga=1' }
 
 ---@class Config
@@ -10,6 +15,7 @@ local Config = {
   font_size = 10.0,
 
   -- NOTE: use fixed otf fonts instead of variable ttf fonts. The latter just don't seem to work properly on wezterm.
+  -- Only installing Italic and Regular seems to work
   font = wezterm.font_with_fallback {
     {
       family = 'Monaspace Neon',
