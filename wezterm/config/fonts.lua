8ead92dc3a268e8e013769abaa6a662456c993e8
chtenb <ctenbrinke@gmail.com>
2026-09-05 11:47:48 +0200

    .

--- wezterm/config/fonts.lua
+++ wezterm/config/fonts.lua
@@ -2,7 +2,7 @@
 local wezterm = require "wezterm"
 
 -- === <= |> =>
-local monaspace_features = { 'calt=1', 'ss01=1', 'ss02=0', 'ss03=1', 'ss04=1', 'ss05=1', 'ss06=1', 'ss07=1', 'ss08=1', 'ss09=0', 'liga=1'
+local monaspace_features = { 'calt=1', 'ss01=1', 'ss02=0', 'ss03=1', 'ss04=1', 'ss05=0', 'ss06=1', 'ss07=1', 'ss08=1', 'ss09=0', 'liga=1'
   , 'cv01=0' -- zero styles
   , 'cv10=0' -- l i alternates (Neon, Argon, Xenon, Radon)
   , 'cv11=0' -- j f r t alternates (Neon, Argon)
